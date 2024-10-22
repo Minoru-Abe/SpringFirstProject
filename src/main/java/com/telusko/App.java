e52ae08af5cbb70f62f7b9f73ad75fe86184ee07
Minoru-Abe <tomominoru.abe@gmail.com>
2024-10-23 08:26:54 +0900

    up to 153

--- src/main/java/com/telusko/App.java
+++ src/main/java/com/telusko/App.java
@@ -1,17 +1,26 @@
 package com.telusko;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.telusko.config.AppConfig;
 
 public class App {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		Alien obj = context.getBean("alien1", Alien.class);
-		obj.code();
-
-		Computer obj2 = context.getBean(Computer.class);
-		obj2.compile();
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		Desktop dt = context.getBean("desktop1", Desktop.class);
+		dt.compile();
+		
+		
+//		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//		Alien obj = context.getBean("alien1", Alien.class);
+//		obj.code();
+//
+//		Computer obj2 = context.getBean(Computer.class);
+//		obj2.compile();
 
 	}
 
